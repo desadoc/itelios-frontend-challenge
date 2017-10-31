313c6905028561c4fa497d5ae88f900ebe19dd4f
Erivaldo Filho <desadoc@gmail.com>
2017-10-31 15:28:27 -0300

    Moved html to template files

--- src/js/cross-sell.js
+++ src/js/cross-sell.js
@@ -1,93 +1,29 @@
 
-function _cross_sell_render_product(product, className) {
-  return '' +
-    '<div class="cross-sell__item ' + className + '">' +
-      '<div class="cross-sell__item__overlay">' +
-        '<a href="#product" class="cross-sell__item__overlay__link"></a>' +
-        '<a href="#addtocart" class="cross-sell__item__cart-btn">' +
-         '<i class="material-icons">add_shopping_cart</i>' +
-        '</a>' +
-      '</div>' +
-      '<span class="cross-sell__item__image">' +
-        '<img src="' + product.imageName + '">' +
-      '</span>' +
-      '<span class="cross-sell__item__description">' +
-        '<div class="cross-sell__item__description__name">' +
-          '<div class="baseline-trick"></div>' +
-          '<div>' + product.name + '</div>' +
-        '</div>' +
-        '<div class="cross-sell__item__description__price">' +
-          'Por: ' +
-          '<span class="cross-sell__item__description__price_tag">' +
-            product.price +
-          '</span>' +
-          '<br />' +
-          'ou até: ' +
-          '<span class="cross-sell__item__description__price_options">' +
-            product.productInfo.paymentConditions +
-          '</span>' +
-        '</div>' +
-      '</span>' +
-    '</div>';
-}
-
-function _cross_sell_render(data) {
-
+function _cross_sell_render(data, templates) {
   var groupSize = 2;
+  var groupCount = Math.ceil(data.recommendation.length/groupSize);
 
-  var viewedItemHtml = _cross_sell_render_product(
-    data.item, 'cross-sell__item--viewed'
-  );
-  var suggestionsHtml = '';
-  var controlBtnsHtml = '';
+  data.item.className = 'cross-sell__item--viewed';
 
   for (var i=0; i<data.recommendation.length; i++) {
-    suggestionsHtml += _cross_sell_render_product(
-      data.recommendation[i], 'cross-sell__item--suggestion'
-    );
+    data.recommendation[i].className = 'cross-sell__item--suggestion';
   }
 
-  var controlBtnsCount = Math.ceil(data.recommendation.length/groupSize);
+  data.buttons = [];
 
-  for (var i=0; i<controlBtnsCount; i++) {
-    controlBtnsHtml += '' +
-      '<li id="cross-sell__btn' + i.toString() + '">' +
-        '<i class="material-icons">lens</i>' +
-      '</li>';
+  for (var i=0; i<groupCount; i++) {
+    data.buttons.push({index: i});
   }
-
-  var htmlValue = '' +
-    '<div class="cross-sell">' +
-      '<div class="cross-sell__viewed">' +
-        '<div class="cross-sell__header">' +
-          '<h3>Você visitou:</h3>' +
-        '</div>' +
-        viewedItemHtml +
-      '</div>' +
-      '<div class="cross-sell__suggestions">' +
-        '<div class="cross-sell__header">' +
-          '<h3>e talvez se interesse por:</h3>' +
-        '</div>' +
-        '<div class="cross-sell__suggestions__items">' +
-          suggestionsHtml +
-        '</div>' +
-      '</div>' +
-      '<div class="cross-sell__controls">' +
-        '<ul class="cross-sell__controls__btnlist">' +
-          controlBtnsHtml +
-        '</ul>' +
-      '</div>' +
-    '</div>';
 
   return {
-    html: htmlValue,
-    groupCount: controlBtnsCount,
+    html: Mustache.render(templates.main, data, {item: templates.item}),
+    groupCount: groupCount,
     currIndex: 0
   };
 }
 
-function _cross_sell_mount(el, data) {
-  var crossSell = _cross_sell_render(data);
+function __cross_sell_mount(el, data, templates) {
+  var crossSell = _cross_sell_render(data, templates);
   el.innerHTML = crossSell.html;
   var timeToTransition = 5000;
 
@@ -139,6 +75,36 @@ function _cross_sell_mount(el, data) {
   crossSell.timer = setTimeout(
     transitionTimerFn, timeToTransition
   );
+}
+
+var _crossSellTemplates = null;
+
+function _cross_sell_mount(el, data) {
+
+  if (_crossSellTemplates) {
+    return __cross_sell_mount(el, data, _crossSellTemplates);
+  }
+
+  var mainTemplatePromise = xhr.get(
+    '/templates/cross-sell.mst'
+  );
+
+  var itemTemplatePromise = xhr.get(
+    '/templates/cross-sell-item.mst'
+  );
+
+  Promise.all([mainTemplatePromise, itemTemplatePromise])
+  .then(function(templates) {
+    _crossSellTemplates = {
+      main: templates[0],
+      item: templates[1]
+    };
+
+    __cross_sell_mount(el, data, _crossSellTemplates);
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
 }
 
 modules.register('cross-sell', {
